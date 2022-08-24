3edef35aa6e0adf96fb338d56ff7567d222251cb
HOLBI <ynechitajlo@holbi.co.uk>
2022-08-24 19:25:06 +0300

    project update version 50612 (removed not used files)

--- lib/backend/design/boxes/include/PromoOnCategory.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (($type ?? null) == 'catalog') {
-  $widgets[] = array('name' => 'catalog\PromoOnCategory', 'title' => 'Promotion on Category', 'description' => '', 'type' => 'catalog');
-}
--- lib/backend/design/boxes/include/SaleCategory.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$params['type'] = $params['type'] ?? null;
-if ($params['type'] != 'email' && $params['type'] != 'invoice' && $params['type'] != 'packingslip' && $params['type'] != 'pdf' && $params['type'] != 'orders') {
-  $widgets[] = array('name' => 'SaleCategory', 'title' => TEXT_SALE_CATEGORY, 'description' => '', 'type' => 'general', 'class' => 'specials-products');
-}
