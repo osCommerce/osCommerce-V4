48d31a361b614cfa66e505d4e8345afd9e30ad79
pandrei <apushkarev@holbi.co.uk>
2024-10-30 14:07:13 -0700

    project update version 63493

--- lib/common/classes/TlMainFileLogWriter.php
+++ lib/common/classes/TlMainFileLogWriter.php
@@ -14,7 +14,39 @@ class TlMainFileLogWriter extends \yii\log\FileTarget
                 $s .= "$key: $info\n";
             }
         }
-        return $s . parent::getContextMessage();
+        
+        $msg = parent::getContextMessage();
+        $msg = preg_replace_callback(
+            "/'email_address' => '(.*)'/",
+            function ($matches) {
+                $email = $matches[1];
+                $start = substr($email, 0, 2);
+                $end = substr($email, -2);
+                $maskedEmail = $start . str_repeat('*', 5) . $end;
+                return "'email_address' => '" . $maskedEmail . "'";
+            },
+            $msg
+        );
+        $msg = preg_replace(
+            "/'password' => '(.*)'/",
+            sprintf("'password' => '%s'", str_repeat('*', 5)),
+            $msg
+        );
+        // remove any other emails
+        $msg = preg_replace_callback(
+            "/([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})/",
+            function ($matches) {
+                $email = $matches[0];
+                $start = substr($email, 0, 2);
+                $end = substr($email, -2);
+                $maskedEmail = $start . str_repeat('*', 5) . $end;
+                return $maskedEmail;
+            },
+            $msg
+        );
+        
+        
+        return $s . $msg;
     }
 
 }
